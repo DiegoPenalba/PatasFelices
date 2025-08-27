d8ba8d9ca342b2af1fde1b99ddac12ee95108b96
DiegoPenalba <dieh.penalba@gmail.com>
2025-08-27 14:30:39 -0300

    Funciones para interfaz de consola

--- PatasFelices/main.py
+++ PatasFelices/main.py
@@ -4,7 +4,7 @@ from clases.mascota import Mascota, Perro, Gato
 from clases.producto import Producto
 from clases.venta import Venta
 
-# funciones para interfaz de consola
+# Funciones para interfaz de consola
 def registrarMascota():
     tipo = input("Ingrese el tipo de mascota (Perro/Gato): ").strip().lower()
     nombre = input("Nombre de la mascota: ")
@@ -20,3 +20,57 @@ def registrarMascota():
         raza = input("Ingrese la raza del gato: ")
         independencia = input("Ingrese el nivel de independencia del gato: ")
         mascota = Gato(nombre, edad, salud, precio, raza, independencia)
+    else:
+        print("Tipo de mascota no reconocido")
+        return
+    
+    return mascota
+
+def registrarCliente():
+    nombre = input("Ingrese nombre del cliente: ")
+    direccion = input("Ingrese direccion del cliente: ")
+    telefono = input("ingrese telefono del cliente: ")
+    cliente = Cliente(nombre, direccion, telefono)
+    return cliente
+
+def registrarVenta(clientes, inventario):
+    nombreCliente = input("Nombre del cliente: ")
+    cliente = next((c for c in clientes if c.nombre == nombreCliente), None)
+    if not cliente:
+        print("Cliente no encontrado.")
+        return
+    
+    productos = []
+    
+    while True:
+        nombreDelProducto = input("Nombre del producto (deje vacio para finalizar): ")
+        if not nombreDelProducto:
+            break
+
+        producto = next((p for p in inventario.listaDeProductos if p.nombre == nombreDelProducto), None)
+        if producto:
+            productos.append(producto)
+        else:
+            print("Producto no encontrado")
+    
+    if productos:
+        venta = Venta(cliente, productos)
+        venta.registrarVenta()
+        print("La venta ha sido realizada con exito")
+    else:
+        print("No se han registrado productos para la venta.")
+
+def mostrarMenu():
+    print("\n --- Menu de gestion de Patas Felices ---")
+    print("1. Registrar Mascota")
+    print("2. Registrar Cliente")
+    print("3. Registrar Producto")
+    print("4. Registrar Venta")
+    print("5. Mostrar informacion de Mascotas")
+    print("6. Mostrar informacion de Clientes")
+    print("7. Mostrar informacion de Productos")
+    print("8. Generar alerta de Inventario")
+    print("9. Salir.")
+
+
+    
